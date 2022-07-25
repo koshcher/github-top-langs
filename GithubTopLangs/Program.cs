41ea331bbef5042f47ea1dfac102ecb66c11b03d
Roman Koshchei <romankoshchei@gmail.com>
2022-07-25 16:06:26 +0300

    ?

--- GithubTopLangs/Program.cs
+++ GithubTopLangs/Program.cs
@@ -1,14 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var portExists = int.TryParse(Environment.GetEnvironmentVariable("PORT"), out var port);
-if (portExists)
-{
-  builder.WebHost.ConfigureKestrel(options =>
-  {
-    options.ListenAnyIP(port);
-  });
-}
 
 // Enable from any origin
 builder.Services.AddCors(options =>
@@ -19,6 +11,12 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+if (app.Environment.IsProduction())
+{
+  var port = Environment.GetEnvironmentVariable("PORT");
+  app.Urls.Add($"http://*:{port}");
+}
 
 if (app.Environment.IsDevelopment())
 {
